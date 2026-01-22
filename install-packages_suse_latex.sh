648e07a033a546ac49993d9cf977f6728de201fd
Michael Adam <obnox@samba.org>
2026-01-22 11:31:10 +0100

    build: additional packages for suse latex images
    Signed-off-by: Michael Adam <obnox@samba.org>

--- install-packages_suse_latex.sh
+++ install-packages_suse_latex.sh
@@ -18,6 +18,8 @@ zypper  install  -y git make cmake  texlive-beamer \
 	texlive-latex-uni8 \
 	texlive-ginpenc \
 	texlive-german \
+	texlive-hyperxmp \
+	texlive-fancyhdr \
 	ghostscript vim less dia
 
 
