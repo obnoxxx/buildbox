4b868b39037088b83d8de04866ba99aaac8239db
Michael Adam <obnox@samba.org>
2026-02-03 20:38:45 +0100

    WIP:images: add verification to suse-c build
    Signed-off-by: Michael Adam <obnox@samba.org>

--- install-packages_suse_c.sh
+++ install-packages_suse_c.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
 
-zypper install  -y git gcc make cmake automake autoconf python3 vim less
 
+		zypper install  -y git gcc make cmake automake autoconf python3 vim less
 
+		RC=$?
+
+		if [ ${RC} -eq 0 ]; then
+				echo "successfully installed packages."
+		else
+				echo "error installing packages."
+        fi
+
+		exit ${RC}
